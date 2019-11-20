59d37e44258dc0ff50df2fd106591f676d4d0330
Rodrigo Morelatto <rodrigo.geof@gmail.com>
2019-11-20 13:21:01 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Numerical example of option games from Havard Business Review article (2009).',
+    author='Rodrigo Morelatto',
+    license='',
+)
